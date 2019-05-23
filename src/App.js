488cecf326d35a8329cb80b4fe0e9a8901084d0f
Benny <carlsson.benny93@gmail.com>
2019-05-23 15:10:49 +0800

    added material-ui and appbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import IconButton from "@material-ui/core/IconButton"
+import Icon from "@material-ui/core/Icon"
+import Fab from "@material-ui/core/Fab"
+import { withStyles } from "@material-ui/core/styles"
 
-function App() {
+const styles = theme => ({
+  appBar: {
+    top: "auto",
+    bottom: 0
+  },
+  toolbar: {
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  fabButton: {
+    position: "absolute",
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: "0 auto"
+  }
+})
+
+function App(props) {
+  const { classes } = props
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton color="inherit" aria-label="Open drawer">
+            <Icon>menu</Icon>
+          </IconButton>
+          <Fab color="secondary" aria-label="Add" className={classes.fabButton}>
+            <Icon>photo_camera</Icon>
+          </Fab>
+        </Toolbar>
+      </AppBar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default withStyles(styles)(App)
