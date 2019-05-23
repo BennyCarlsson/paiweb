7022a9f000af91eb5b16aaa6071f63b7a7c9e6ae
Benny <carlsson.benny93@gmail.com>
2019-05-23 17:17:38 +0800

    Updates

---
+++ precache-manifest.3f6f1abb20fa972a9d3d17f4f2e11e6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61463dd48ded7416b59e400195ac7242",
+    "url": "/paiweb/index.html"
+  },
+  {
+    "revision": "b4392e2ad5922ed09614",
+    "url": "/paiweb/static/css/main.0f258fc9.chunk.css"
+  },
+  {
+    "revision": "b6ca43b7297c600a0c23",
+    "url": "/paiweb/static/js/2.2c07e042.chunk.js"
+  },
+  {
+    "revision": "b4392e2ad5922ed09614",
+    "url": "/paiweb/static/js/main.8e5faf49.chunk.js"
+  },
+  {
+    "revision": "e9d81b096f34e7b99288",
+    "url": "/paiweb/static/js/runtime~main.f48f10f2.js"
+  }
+]);
