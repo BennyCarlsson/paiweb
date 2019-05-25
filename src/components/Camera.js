e1432a152f760e041be9bc3c4fca96198120179d
Benny <carlsson.benny93@gmail.com>
2019-05-25 19:45:07 +0800

    code cleanup

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -3,21 +3,10 @@ import ExifOrientationImg from "react-exif-orientation-img"
 
 const Camera = props => {
   const inputRef = createRef()
-  const [state, setState] = useState({ file: {}, imagePreviewUrl: "" })
+  const [imagePreviewUrl, setimagePreviewUrl] = useState("")
 
   const handleFile = event => {
-    // let reader = new FileReader()
-    // let file = event.target.files[0]
-
-    // reader.onloadend = () => {
-    //   setState({
-    //     file: file,
-    //     imagePreviewUrl: reader.result
-    //   })
-    // }
-
-    // reader.readAsDataURL(file)
-    setState({ imagePreviewUrl: URL.createObjectURL(event.target.files[0]) })
+    setimagePreviewUrl(URL.createObjectURL(event.target.files[0]))
   }
 
   return (
@@ -33,10 +22,9 @@ const Camera = props => {
         styles={"image-orientation: from-image"}
         width="250px"
         height="250px"
-        src={state.imagePreviewUrl}
+        src={imagePreviewUrl}
         alt="preview"
       />
-      <p>asd</p>
     </div>
   )
 }
--- src/components/CustomBottomAppBar.js
+++ src/components/CustomBottomAppBar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { withStyles } from "@material-ui/core/styles"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
@@ -7,42 +7,18 @@ import Fab from "@material-ui/core/Fab"
 import CustomSideDrawer from "./CustomSideDrawer"
 import Icon from "@material-ui/core/Icon"
 
-const styles = theme => ({
-  appBar: {
-    top: "auto",
-    bottom: 0
-  },
-  toolbar: {
-    alignItems: "center",
-    justifyContent: "space-between"
-  },
-  fabButton: {
-    position: "absolute",
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: "0 auto"
-  }
-})
-
 function BottomAppBar(props) {
   const { classes } = props
 
-  const [state, setState] = React.useState({
-    openSideDrawer: false
-  })
+  const [openSideDrawer, setOpenSideDrawer] = useState(false)
 
   const toggleDrawer = openSideDrawer => () => {
-    setState({ openSideDrawer: openSideDrawer })
+    setOpenSideDrawer(openSideDrawer)
   }
 
   return (
     <span>
-      <CustomSideDrawer
-        open={state.openSideDrawer}
-        toggleDrawer={toggleDrawer}
-      />
+      <CustomSideDrawer open={openSideDrawer} toggleDrawer={toggleDrawer} />
       <AppBar position="fixed" color="primary" className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
           <IconButton
@@ -65,5 +41,24 @@ function BottomAppBar(props) {
     </span>
   )
 }
+
+const styles = theme => ({
+  appBar: {
+    top: "auto",
+    bottom: 0
+  },
+  toolbar: {
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  fabButton: {
+    position: "absolute",
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: "0 auto"
+  }
+})
 
 export default withStyles(styles)(BottomAppBar)
