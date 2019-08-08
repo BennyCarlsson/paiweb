5fee5a6c38f29642de4058791baa62fe715649c3
Benny <carlsson.benny93@gmail.com>
2019-08-08 13:31:30 +0200

    Updates

---
+++ precache-manifest.a5c5ed7e7c2be1fbbcbbe77abb606a1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431d34e08fa48b25b4669bc48580fb94",
+    "url": "/paiweb/index.html"
+  },
+  {
+    "revision": "e617cfa203498e219d24",
+    "url": "/paiweb/static/css/main.6abf1fa2.chunk.css"
+  },
+  {
+    "revision": "901e583b9b7dbecd69b0",
+    "url": "/paiweb/static/js/2.386cca41.chunk.js"
+  },
+  {
+    "revision": "e617cfa203498e219d24",
+    "url": "/paiweb/static/js/main.c773863d.chunk.js"
+  },
+  {
+    "revision": "e9d81b096f34e7b99288",
+    "url": "/paiweb/static/js/runtime~main.f48f10f2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/paiweb/precache-manifest.f5c5d1c82350597de8f80c5678b29a10.js"
+  "/paiweb/precache-manifest.a5c5ed7e7c2be1fbbcbbe77abb606a1a.js"
 );
 
 self.addEventListener('message', (event) => {
