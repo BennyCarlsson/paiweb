050682080c0d40b44e28f7184b6efc8ffa94a63b
Benny <carlsson.benny93@gmail.com>
2019-08-10 18:04:52 +0200

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,34 +7,32 @@ let config = {
 firebase.initializeApp(config)
 
 const messaging = firebase.messaging()
-
-// messaging.setBackgroundMessageHandler(payload => {
-//   const title = payload.notification.title
-//   console.log("payload", payload.notification.icon)
-//   const options = {
-//     body: payload.notification.body,
-//     icon: payload.notification.icon
-//   }
-//   return self.registration.showNotification(title, options)
-// })
-
-messaging.setBackgroundMessageHandler(function(payload) {
-  const promiseChain = clients
-    .matchAll({
-      type: "window",
-      includeUncontrolled: true
-    })
-    .then(windowClients => {
-      for (let i = 0; i < windowClients.length; i++) {
-        const windowClient = windowClients[i]
-        windowClient.postMessage(payload)
-      }
-    })
-    .then(() => {
-      return registration.showNotification("my notification title")
-    })
-  return promiseChain
+messaging.setBackgroundMessageHandler(payload => {
+  const title = payload.data.title
+  const options = {
+    body: payload.data.body,
+    icon: payload.data.icon
+  }
+  return self.registration.showNotification(title, options)
 })
+
+// messaging.setBackgroundMessageHandler(function(payload) {
+//   const promiseChain = clients
+//     .matchAll({
+//       type: "window",
+//       includeUncontrolled: true
+//     })
+//     .then(windowClients => {
+//       for (let i = 0; i < windowClients.length; i++) {
+//         const windowClient = windowClients[i]
+//         windowClient.postMessage(payload)
+//       }
+//     })
+//     .then(() => {
+//       return registration.showNotification("my notification title")
+//     })
+//   return promiseChain
+// })
 
 //Todo https://firebase.google.com/docs/cloud-messaging/js/send-multiple
 //onTokenRefresh
