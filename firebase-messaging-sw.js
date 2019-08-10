bea2ae40b987f06cd34c739e4e5baa8c0169e721
Benny <carlsson.benny93@gmail.com>
2019-08-10 15:28:07 +0200

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,15 +5,35 @@ let config = {
   messagingSenderId: "560094021764"
 }
 firebase.initializeApp(config)
+
 const messaging = firebase.messaging()
-messaging.setBackgroundMessageHandler(payload => {
-  const title = payload.notification.title
-  console.log("payload", payload.notification.icon)
-  const options = {
-    body: payload.notification.body,
-    icon: payload.notification.icon
+
+// messaging.setBackgroundMessageHandler(payload => {
+//   const title = payload.notification.title
+//   console.log("payload", payload.notification.icon)
+//   const options = {
+//     body: payload.notification.body,
+//     icon: payload.notification.icon
+//   }
+//   return self.registration.showNotification(title, options)
+// })
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  )
+  // Customize notification here
+  const notificationTitle = "Background Message Title"
+  const notificationOptions = {
+    body: "Background Message body.",
+    icon: "/ic_launcher.png"
   }
-  return self.registration.showNotification(title, options)
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  )
 })
 
 //Todo https://firebase.google.com/docs/cloud-messaging/js/send-multiple
