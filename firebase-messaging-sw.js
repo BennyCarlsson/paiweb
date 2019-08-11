ec9e460e3bf46417c4213f7d1c946f11333cfbb3
Benny <carlsson.benny93@gmail.com>
2019-08-11 15:40:23 +0200

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,6 +17,25 @@ messaging.setBackgroundMessageHandler(payload => {
   return self.registration.showNotification(title, options)
 })
 
+self.addEventListener("notificationclick", function(event) {
+  //For root applications: just change "'./'" to "'/'"
+  //Very important having the last forward slash on "new URL('./', location)..."
+  const rootUrl = new URL("./", location).href
+  event.notification.close()
+  event.waitUntil(
+    clients.matchAll().then(matchedClients => {
+      for (let client of matchedClients) {
+        if (client.url.indexOf(rootUrl) >= 0) {
+          return client.focus()
+        }
+      }
+
+      return clients.openWindow(rootUrl).then(function(client) {
+        client.focus()
+      })
+    })
+  )
+})
 // messaging.setBackgroundMessageHandler(function(payload) {
 //   const promiseChain = clients
 //     .matchAll({
